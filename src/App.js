c364ed9a64fbb8b61e24d41c7df112a61faeba55
aSebastianMP <strog35777@gmail.com>
2021-03-02 15:34:26 -0600

    commitcuatro

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Google Maps App</h1>
+      <h1>Sebastian Marquez</h1>
       <GoogleMap />
       <Helmet>
         <script defer data-key="a5d02b98-89ba-4c7c-9922-e401eb9e56fa" src="https://widget.tochat.be/bundle.js"></script>
