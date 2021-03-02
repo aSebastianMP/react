16ce1a25878656485eb910db70a77e3dba4900d4
aSebastianMP <strog35777@gmail.com>
2021-03-02 15:40:14 -0600

    commitsiete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <h1>Google Maps App</h1>
       <h1>Sebastian Marquez</h1>
-      <h1>Universidad Tecnologica</h1>
+      <h1>Universidad Tecnologica de Durango</h1>
       <GoogleMap />
       <Helmet>
         <script defer data-key="a5d02b98-89ba-4c7c-9922-e401eb9e56fa" src="https://widget.tochat.be/bundle.js"></script>
